0dc981dc23112b062a3901880c28f1d9033e7bd3
defmementomori <defmementomori@gmail.com>
2022-01-15 16:57:58 +0900

    Update src/resources/chatbot-role.ts
    Co-authored-by: Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>

--- src/resources/chatbot-role.ts
+++ src/resources/chatbot-role.ts
@@ -2,7 +2,7 @@ import { aws_iam as iam, PhysicalName } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export const chatbotRole = (scope: Construct, id: string): iam.Role => {
-  const role = new iam.Role(scope, id, {
+  const role = new iam.Role(scope, 'role', {
     assumedBy: new iam.CompositePrincipal(
       new iam.ServicePrincipal('chatbot.amazonaws.com'),
     ),
